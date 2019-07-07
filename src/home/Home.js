29247a4f6f71674bef82ea287d5655f65b7e0256
buharov-alexander <buharovsasha@yandex.ru>
2019-07-07 23:24:33 +0700

    add b-animate-slide

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,9 +8,11 @@ const Home = () => (
     <Container className="b-home__container">
       <Row className="b-home__row align-items-center">
         <Col className="b-home__text-block">
-          <h3 className="b-home__text b-home__text_h3">Hello! My name is</h3>
-          <h1 className="b-home__text b-home__text_h1">Bukharov Alexander</h1>
-          <h5 className="b-home__text b-home__text_h5 b-home__text_last">Welcome to my personal page!</h5>
+          <div className="b-animate-slide">
+            <h3 className="b-home__text b-home__text_h3">Hello! My name is</h3>
+            <h1 className="b-home__text b-home__text_h1">Bukharov Alexander</h1>
+            <h5 className="b-home__text b-home__text_h5 b-home__text_last">Welcome to my personal page!</h5>
+          </div>
         </Col>
       </Row>
     </Container>
