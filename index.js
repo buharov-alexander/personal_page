fcfae4c1b0837ec1378060a93d2c0a1745970e5b
buharov-alexander <buharovsasha@yandex.ru>
2020-05-01 13:29:42 +0700

    page with fullpage.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const myFullpage = new fullpage('#fullpage', {
+  menu: '#menu',
+  anchors: ['Home', 'Biography', 'Skills'],
+  sectionsColor: ['#00473e', '#1BBC9B', '#7E8F7C'],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['Home', 'Biography', 'Skills']
+});
