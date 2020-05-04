5252480975566f21b07d0af4a79bddcd93d19f2e
buharov-alexander <buharovsasha@yandex.ru>
2020-05-04 12:32:20 +0700

    move style to scss

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const myFullpage = new fullpage('#fullpage', {
   menu: '#menu',
   anchors: ['Home', 'Biography', 'Skills'],
-  sectionsColor: ['#00473e', '#1BBC9B', '#7E8F7C'],
+  sectionsColor: ['#f2f7f5', '#1BBC9B', '#7E8F7C'],
   navigation: true,
-  navigationPosition: 'right',
+  navigationPosition: 'left',
   navigationTooltips: ['Home', 'Biography', 'Skills']
 });
