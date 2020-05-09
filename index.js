26d9e78c29d6e34a23871f552699ee66e86d3e57
buharov-alexander <buharovsasha@yandex.ru>
2020-05-10 00:58:04 +0700

    add common backend element

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const myFullpage = new fullpage('#fullpage', {
   menu: '#menu',
   anchors: ['Home', 'Biography', 'Skills'],
-  sectionsColor: ['#f2f7f5', '#1BBC9B', '#7E8F7C'],
   navigation: true,
   navigationPosition: 'left',
   navigationTooltips: ['Home', 'Biography', 'Skills']
