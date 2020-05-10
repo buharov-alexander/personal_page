995d2b121f36dbb8f0902b02359f116830020454
buharov-alexander <buharovsasha@yandex.ru>
2020-05-10 13:13:52 +0700

    move backend

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const sections = ["section-1", "section-2", "section-3"];
+
 const myFullpage = new fullpage('#fullpage', {
   menu: '#menu',
   anchors: ['Home', 'Biography', 'Skills'],
   navigation: true,
   navigationPosition: 'left',
-  navigationTooltips: ['Home', 'Biography', 'Skills']
+  navigationTooltips: ['Home', 'Biography', 'Skills'],
+  afterLoad: function (origin, destination, direction) {
+    const backend = document.getElementById("backend-element");
+    backend.classList.remove(sections[origin.index]);
+    backend.classList.add(sections[destination.index]);  
+  }
 });
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   const loader = document.querySelector('#loader');
-  loader.className += ' loader-hidden';
+  loader.classList.add('loader-hidden');
 });
